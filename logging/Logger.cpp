7e8929a9ffefade335d97ab3a9288190c9a899a1
Trompettesib <alexandre.tissot@epitech.eu>
2022-09-10 21:10:33 +0200

    Add comments, a constructor, and const keywords

--- logging/Logger.cpp
+++ logging/Logger.cpp
@@ -9,6 +9,9 @@
 
 namespace logging
 {
+    /**
+     * @brief Construct a new Logger:: Logger object
+     */
     Logger::Logger()
     {
         this->create_folder_if_nessessary();
@@ -16,6 +19,15 @@ namespace logging
         this->_file_stream.open(this->_file_path, std::ios::app);
     }
 
+    /**
+     * @brief Construct a new Logger:: Logger object
+     *
+     * @param file_name Name of the log file
+     *
+     * @overload Logger::Logger()
+     *
+     * @note The file will be created as well as the folder
+     */
     Logger::Logger(std::string file_name)
     {
         this->create_folder_if_nessessary();
@@ -23,6 +35,17 @@ namespace logging
         this->_file_stream.open(this->_file_path, std::ios::app);
     }
 
+    /**
+     * @brief Construct a new Logger:: Logger object
+     *
+     * @param folder_name Name of the folder where the log file will be stored
+     * @param file_name Name of the log file
+     *
+     * @overload Logger::Logger()
+     *
+     *
+     * @note The file will be created as well as the folder
+     */
     Logger::Logger(std::string folder_name, std::string file_name)
     {
         this->create_folder_if_nessessary(folder_name);
@@ -30,11 +53,40 @@ namespace logging
         this->_file_stream.open(this->_file_path, std::ios::app);
     }
 
+    /**
+     * @brief Construct a new Logger:: Logger object
+     *
+     * @param create_all If true, the folder and the file will be created
+     *
+     * @overload Logger::Logger()
+     *
+     * @note The file will be created as well as the folder
+     */
+    Logger::Logger(bool create_all)
+    {
+        if (create_all)
+        {
+            this->create_folder_if_nessessary();
+            this->create_file();
+            this->_file_stream.open(this->_file_path, std::ios::app);
+        }
+    }
+
+    /**
+     * @brief Destroy the Logger:: Logger object
+     *
+     * @note The file stream will be closed
+     */
     Logger::~Logger()
     {
         this->_file_stream.close();
     }
 
+    /**
+     * @brief Create the folder if it doesn't exist
+     *
+     * @param folder_name Name of the folder to create. This parameter is optional and will be set to "logs" by default
+     */
     void Logger::create_folder_if_nessessary(std::string folder_name)
     {
         this->_folder_path = folder_name;
@@ -74,6 +126,11 @@ namespace logging
         }
     }
 
+    /**
+     * @brief Create the file
+     *
+     * @param filename Name of the file to create. This parameter is optional and will be set to the current date by default with an indexer if the file already exist
+     */
     void Logger::create_file(std::string filename)
     {
         this->_file_path = this->_folder_path + '/' + filename;
@@ -87,7 +144,15 @@ namespace logging
         std::cout << "log file: " << this->_file_path << std::endl;
     }
 
-    bool Logger::file_exist(std::string filename)
+    /**
+     * @brief Check if the file exist
+     *
+     * @param filename Name of the file to check
+     *
+     * @return true The file exist
+     * @return false The file doesn't exist
+     */
+    bool Logger::file_exist(std::string filename) const
     {
         std::fstream file;
         file.open(filename);
@@ -99,7 +164,14 @@ namespace logging
         return false;
     }
 
-    std::string Logger::get_time()
+    /**
+     * @brief Get the current time
+     *
+     * @return std::string The current time
+     *
+     * @note The format is YYYY/MM/DD HH:MM:SS.sss by default
+     */
+    std::string Logger::get_time() const
     {
         const std::time_t now = std::time(nullptr);
         const std::tm* tm_now = std::localtime(&now);
@@ -115,7 +187,18 @@ namespace logging
                 std::to_string(millis);
     }
 
-    std::string Logger::get_time(std::string format)
+    /**
+     * @brief Get the current time
+     *
+     * @param format Format of the time. When there is YYYY or any other format letters, they will be replaced by the current time
+     *
+     * @return std::string The current time
+     *
+     * @overload std::string Logger::get_time() const
+     *
+     * @note The format is YYYY/MM/DD HH:MM:SS.sss by default
+     */
+    std::string Logger::get_time(std::string format) const
     {
         const std::time_t now = std::time(nullptr);
         const std::tm* tm_now = std::localtime(&now);
@@ -137,6 +220,13 @@ namespace logging
         return format;
     }
 
+    /**
+     * @brief Write a message in the log file
+     *
+     * @param message Message to write
+     *
+     * @note The message will be written in the log file with the current time
+     */
     void Logger::log(std::string message)
     {
         this->_file_stream << this->get_time("YYYY/MM/DD HH:mm:SS.sss") << " " << message << std::endl;
--- logging/Logger.hpp
+++ logging/Logger.hpp
@@ -6,23 +6,28 @@
 
 namespace logging
 {
+    /**
+     * @brief Handles logging in a file, creating the file if it doesn't exist and the folder too
+     */
     class Logger
     {
     public:
         Logger();
         Logger(std::string file_name);
         Logger(std::string folder_name, std::string file_name);
+        Logger(bool create_all);
         ~Logger();
         void log(std::string message);
+
     private:
         void create_folder_if_nessessary(std::string folder_name = "logs");
         void create_file(std::string filename = "");
-        bool file_exist(std::string filename);
-        std::string get_time();
-        std::string get_time(std::string format);
-        std::string _folder_path;
-        std::string _file_path;
-        std::fstream _file_stream;
+        bool file_exist(std::string filename) const;
+        std::string get_time() const;
+        std::string get_time(std::string format) const;
+        std::string _folder_path;   /// Path to the folder where log files are stored
+        std::string _file_path;     /// Path to the current log file
+        std::fstream _file_stream;  /// Stream to the current log file
     };
 }
 
