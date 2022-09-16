a9e42145882a2bbd73d7cc0c068461755aa06cef
Ode Marchand <62648027+NekoSelena@users.noreply.github.com>
2022-09-16 13:30:18 +0200

    File setup

---
+++ configuration/ConfigHandler.cpp
@@ -0,0 +1,2 @@
+#include "ConfigHandler.hpp"
+
---
+++ configuration/ConfigHandler.hpp
@@ -0,0 +1,21 @@
+#ifndef CONFIG_CONFIGHANDLER_HPP
+#define CONFIG_CONFIGHANDLER_HPP
+
+namespace Configuration
+{
+
+    /**
+     * @brief Handles the server configuration
+     * by parsing the config.yml file
+     */
+    class ConfigHandler
+    {
+    public:
+        ConfigHandler();
+        ~ConfigHandler();
+    private:
+        /* empty for now */;
+    };
+}
+
+#endif /* CONFIG_CONFIGHANDLER_HPP */
