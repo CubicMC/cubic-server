8e7870611be1a618c0720a3da58518f5299f6928
NekoSelena <ode.marchand@epitech.eu>
2022-09-25 00:38:49 +0200

    generalization with getNode + kind of error handling

--- configuration/ConfigHandler.cpp
+++ configuration/ConfigHandler.cpp
@@ -12,39 +12,35 @@ Configuration::ConfigHandler::~ConfigHandler(){}
 
 void Configuration::ConfigHandler::getConfigFile(std::string configFile)
 {
-    YAML::Node config = YAML::LoadFile(configFile);
+    try
+    {
+        YAML::Node config = YAML::LoadFile(configFile);
 
-    const std::string ip = config["network"]["ip"].as<std::string>();
-    const int port = config["network"]["port"].as<int>();
-    const int max_players = config["general"]["max_players"].as<int>();
-    const std::string motd = config["general"]["motd"].as<std::string>();
+        const std::string ip = config["network"]["ip"].as<std::string>();
+        const int port = config["network"]["port"].as<int>();
+        const int max_players = config["general"]["max_players"].as<int>();
+        const std::string motd = config["general"]["motd"].as<std::string>();
 
-    this->_configFile.insert(std::pair<std::string, std::string>("ip", ip));
-    this->_configFile.insert(std::pair<std::string, std::string>("port", std::to_string(port)));
-    this->_configFile.insert(std::pair<std::string, std::string>("max_players", std::to_string(max_players)));
-    this->_configFile.insert(std::pair<std::string, std::string>("motd", motd));
+        this->_configFile.insert({"ip", ip});
+        this->_configFile.insert({"port", std::to_string(port)});
+        this->_configFile.insert({"max_players", std::to_string(max_players)});
+        this->_configFile.insert({"motd", motd});
+    }
+    catch(const std::exception& e)
+    {
+        std::cerr << e.what() << '\n';
+    }
 }
 
-std::string Configuration::ConfigHandler::getIP(void)
+std::string Configuration::ConfigHandler::getNode(std::string node_name)
 {
-    std::string value = this->_configFile.find("ip")->second;
-    return value;
-}
-
-std::string Configuration::ConfigHandler::getMotd(void)
-{
-    std::string value = this->_configFile.find("motd")->second;
-    return value;
-}
-
-std::string Configuration::ConfigHandler::getPort(void)
-{
-    std::string value = this->_configFile.find("port")->second;
-    return value;
-}
-
-std::string Configuration::ConfigHandler::getMaxPlayers(void)
-{
-    std::string value = this->_configFile.find("max_players")->second;
-    return value;
+    try
+    {
+        std::string value = this->_configFile.find(node_name)->second;
+        return value;
+    }
+    catch(const std::exception& e)
+    {
+        std::cerr << e.what() << '\n';
+    }
 }
