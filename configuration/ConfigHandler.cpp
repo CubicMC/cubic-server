aeeb075c81d0a2e64e4966a674d87595a5d5df48
NekoSelena <ode.marchand@epitech.eu>
2022-09-25 01:10:00 +0200

    error handling + fix include

--- configuration/ConfigHandler.cpp
+++ configuration/ConfigHandler.cpp
@@ -1,7 +1,7 @@
 #include "ConfigHandler.hpp"
 #include <iostream>
 #include <fstream>
-#include "../yaml-cpp/include/yaml-cpp/yaml.h"
+#include <yaml-cpp/yaml.h>
 
 Configuration::ConfigHandler::ConfigHandler(std::string filePath)
 {
@@ -10,35 +10,45 @@ Configuration::ConfigHandler::ConfigHandler(std::string filePath)
 
 void Configuration::ConfigHandler::getConfigFile(std::string configFile)
 {
+    YAML::Node config;
+    
     try
     {
-        YAML::Node config = YAML::LoadFile(configFile);
-
-        const std::string ip = config["network"]["ip"].as<std::string>();
-        const int port = config["network"]["port"].as<int>();
-        const int max_players = config["general"]["max_players"].as<int>();
-        const std::string motd = config["general"]["motd"].as<std::string>();
-
-        this->_configFile.insert({"ip", ip});
-        this->_configFile.insert({"port", std::to_string(port)});
-        this->_configFile.insert({"max_players", std::to_string(max_players)});
-        this->_configFile.insert({"motd", motd});
+        config = YAML::LoadFile(configFile);
     }
-    catch(const std::exception& e)
+    catch(const YAML::BadFile& e)
     {
         std::cerr << e.what() << '\n';
     }
+
+    // don't know how to generalize this
+    const std::string ip = config["network"]["ip"].as<std::string>();
+    const int port = config["network"]["port"].as<int>();
+    const int max_players = config["general"]["max_players"].as<int>();
+    const std::string motd = config["general"]["motd"].as<std::string>();
+
+    this->_configFile.insert({"ip", ip});
+    this->_configFile.insert({"port", std::to_string(port)});
+    this->_configFile.insert({"max_players", std::to_string(max_players)});
+    this->_configFile.insert({"motd", motd});
 }
 
 std::string Configuration::ConfigHandler::getNode(std::string node_name)
 {
+    std::string value;
+
     try
     {
-        std::string value = this->_configFile.find(node_name)->second;
-        return value;
+        if (this->_configFile.find(node_name) != this->_configFile.end()) {
+            value = this->_configFile.find(node_name)->second;
+            return value;
+        } else {
+            throw invalid_argument("This node does not exist.");
+        }
     }
-    catch(const std::exception& e)
+    catch(const std::invalid_argument& e)
     {
         std::cerr << e.what() << '\n';
+        return nullptr;
     }
 }
