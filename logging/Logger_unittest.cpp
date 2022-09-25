5ad0d5c4ed6d424ad6f0de32f9ff5afe1459a0bc
Trompettesib <alexandre.tissot@epitech.eu>
2022-09-25 04:10:45 +0200

    Add some test to the logging about the last changes

--- logging/Logger_unittest.cpp
+++ logging/Logger_unittest.cpp
@@ -6,7 +6,7 @@
 
 namespace logging
 {
-    TEST(Logging, log)
+    TEST(Logging, default_behavior)
     {
         Logger logger;
         FileAndFolderHandler ff_handler;
@@ -15,14 +15,146 @@ namespace logging
         EXPECT_TRUE(ff_handler.folder_exist("logs"));
         EXPECT_TRUE(ff_handler.file_exist("logs/" + filename));
 
+        testing::internal::CaptureStdout();
         logger.log(LogLevel::DEBUG, "test");
+        std::string output = testing::internal::GetCapturedStdout();
+
         std::fstream file("logs/" + filename, std::ios::in);
         std::string line;
         std::getline(file, line);
 
         EXPECT_NE(line.find(TimeFormatter::get_time("[YYYY/MM/DD HH:mm:SS")), std::string::npos);
+        EXPECT_NE(output.find(TimeFormatter::get_time("[YYYY/MM/DD HH:mm:SS")), std::string::npos);
         EXPECT_NE(line.find("[DEBUG]"), std::string::npos);
+        EXPECT_NE(output.find("[DEBUG]"), std::string::npos);
         EXPECT_NE(line.find("test"), std::string::npos);
+        EXPECT_NE(output.find("test"), std::string::npos);
+
+        file.close();
+        filename = "logs/" + filename;
+        std::remove(filename.c_str());
+        rmdir("logs");
+    }
+
+    TEST(Logging, vector_specification_level_in_file)
+    {
+        Logger logger;
+        std::string filename = TimeFormatter::get_time("YYYY-MM-DD-1.log");
+        std::vector<LogLevel> levels = {LogLevel::DEBUG, LogLevel::INFO, LogLevel::WARNING, LogLevel::ERROR, LogLevel::FATAL};
+
+        EXPECT_EQ(logger.get_display_specification_level_in_file(), levels);
+        logger.set_display_specification_level_in_file(LogLevel::DEBUG);
+        levels.push_back(LogLevel::DEBUG);
+        EXPECT_EQ(logger.get_display_specification_level_in_file(), levels);
+        logger.unset_display_specification_level_in_file(LogLevel::FATAL);
+        levels.erase(levels.end() - 2);
+        EXPECT_EQ(logger.get_display_specification_level_in_file(), levels);
+
+        filename = "logs/" + filename;
+        std::remove(filename.c_str());
+        rmdir("logs");
+    }
+
+    TEST(Logging, vector_specification_level_in_console)
+    {
+        Logger logger;
+        std::string filename = TimeFormatter::get_time("YYYY-MM-DD-1.log");
+        std::vector<LogLevel> levels = {LogLevel::DEBUG, LogLevel::INFO, LogLevel::WARNING, LogLevel::ERROR, LogLevel::FATAL};
+
+        EXPECT_EQ(logger.get_display_specification_level_in_console(), levels);
+        logger.set_display_specification_level_in_console(LogLevel::DEBUG);
+        levels.push_back(LogLevel::DEBUG);
+        EXPECT_EQ(logger.get_display_specification_level_in_console(), levels);
+        logger.unset_display_specification_level_in_console(LogLevel::FATAL);
+        levels.erase(levels.end() - 2);
+        EXPECT_EQ(logger.get_display_specification_level_in_console(), levels);
+
+        filename = "logs/" + filename;
+        std::remove(filename.c_str());
+        rmdir("logs");
+    }
+
+    TEST(Logging, display_specification_level_in_file)
+    {
+        Logger logger;
+        FileAndFolderHandler ff_handler;
+        std::string filename = TimeFormatter::get_time("YYYY-MM-DD-1.log");
+
+        testing::internal::CaptureStdout();
+        logger.log(LogLevel::DEBUG, "test");
+        std::string output = testing::internal::GetCapturedStdout();
+
+        std::fstream file("logs/" + filename, std::ios::in);
+        std::string line;
+        std::getline(file, line);
+
+        EXPECT_NE(line.find("test"), std::string::npos);
+        EXPECT_NE(output.find("test"), std::string::npos);
+
+        logger.unset_display_specification_level_in_file(LogLevel::DEBUG);
+
+        testing::internal::CaptureStdout();
+        logger.log(LogLevel::DEBUG, "not_a_test");
+        output = testing::internal::GetCapturedStdout();
+        std::getline(file, line);
+
+        EXPECT_EQ(line.find("not_a_test"), std::string::npos);
+        EXPECT_NE(output.find("not_a_test"), std::string::npos);
+
+        // file.seekg(0, std::ios::beg);
+        file.close();
+        file.open("logs/" + filename, std::ios::in);
+        std::getline(file, line);
+
+        testing::internal::CaptureStdout();
+        logger.log(LogLevel::FATAL, "fatal_test");
+        output = testing::internal::GetCapturedStdout();
+        std::getline(file, line);
+        std::cout << "line = " << line << std::endl;
+
+        EXPECT_NE(line.find("fatal_test"), std::string::npos);
+        EXPECT_NE(output.find("fatal_test"), std::string::npos);
+
+        file.close();
+        filename = "logs/" + filename;
+        std::remove(filename.c_str());
+        rmdir("logs");
+    }
+
+    TEST(Logging, display_specification_level_in_console)
+    {
+        Logger logger;
+        FileAndFolderHandler ff_handler;
+        std::string filename = TimeFormatter::get_time("YYYY-MM-DD-1.log");
+
+        testing::internal::CaptureStdout();
+        logger.log(LogLevel::DEBUG, "test");
+        std::string output = testing::internal::GetCapturedStdout();
+
+        std::fstream file("logs/" + filename, std::ios::in);
+        std::string line;
+        std::getline(file, line);
+
+        EXPECT_NE(line.find("test"), std::string::npos);
+        EXPECT_NE(output.find("test"), std::string::npos);
+
+        logger.unset_display_specification_level_in_console(LogLevel::DEBUG);
+
+        testing::internal::CaptureStdout();
+        logger.log(LogLevel::DEBUG, "not_a_test");
+        output = testing::internal::GetCapturedStdout();
+        std::getline(file, line);
+
+        EXPECT_NE(line.find("not_a_test"), std::string::npos);
+        EXPECT_EQ(output.find("not_a_test"), std::string::npos);
+
+        testing::internal::CaptureStdout();
+        logger.log(LogLevel::FATAL, "fatal_test");
+        output = testing::internal::GetCapturedStdout();
+        std::getline(file, line);
+
+        EXPECT_NE(line.find("fatal_test"), std::string::npos);
+        EXPECT_NE(output.find("fatal_test"), std::string::npos);
 
         file.close();
         filename = "logs/" + filename;
