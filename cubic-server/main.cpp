b1348215e4a6dd7149a6948aa50b005fd641b707
Alexandre Flion <dev@huntears.com>
2022-09-25 02:16:09 +0200

    Remove testing code

--- cubic-server/main.cpp
+++ cubic-server/main.cpp
@@ -11,13 +11,6 @@ static void print_usage(const char *caller)
 
 int main(int argc, char **argv)
 {
-    std::vector<uint8_t> test = {0x5, 0x1, 'h', 0x69, 0x69, 0x1};
-    std::shared_ptr<protocol::Handshake> result = protocol::parseHandshake(test);
-    std::cout << "prot version: " << result->prot_version << std::endl;
-    std::cout << "addr: " << result->addr << std::endl;
-    std::cout << "port: " << result->port << std::endl;
-    std::cout << "next state: " << result->next_state << std::endl;
-    return 0;
     if (argc >= 2 && !strcmp(argv[1], "-h"))
     {
         print_usage(argv[0]);
