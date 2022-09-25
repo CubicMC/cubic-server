a1cf498cebc7ff3313d888150e0368c37e61468f
NekoSelena <ode.marchand@epitech.eu>
2022-09-25 05:43:36 +0200

    generalization + error handling

--- configuration/ConfigHandler.cpp
+++ configuration/ConfigHandler.cpp
@@ -8,47 +8,57 @@ Configuration::ConfigHandler::ConfigHandler(std::string filePath)
     this->getConfigFile(filePath);
 }
 
+void Configuration::ConfigHandler::parseFile(YAML::Node base_node)
+{
+    std::vector<YAML::Node> nodes_list;
+    int i = 0;
+
+    for (YAML::const_iterator it = base_node.begin(); it != base_node.end(); ++it, ++i) {
+        nodes_list.push_back(it->first);
+        const std::string buff = it->first.as<std::string>();
+        this->_configFile.insert(std::pair<std::string, YAML::Node>(buff, base_node[nodes_list[i]]));
+        if (base_node[nodes_list.back()].size() > 1) {
+            for (YAML::const_iterator itbis = base_node[nodes_list[i]].begin(); itbis != base_node[nodes_list[i]].end(); ++itbis) {
+                YAML::Node node = itbis->first;
+                const std::string temp = itbis->first.as<std::string>();
+                this->_configFile.insert(std::pair<std::string, YAML::Node>(temp, base_node[nodes_list[i]][node]));
+                parseFile(base_node[nodes_list[i]][node]);
+            }
+        } else {
+            const std::string temp = it->first.as<std::string>();
+            this->_configFile.insert(std::pair<std::string, YAML::Node>(temp, base_node[nodes_list[i]]));
+        }
+    }
+}
+
 void Configuration::ConfigHandler::getConfigFile(std::string configFile)
 {
-    YAML::Node config;
-    
     try
     {
-        config = YAML::LoadFile(configFile);
+        YAML::Node config = YAML::LoadFile(configFile);
+        parseFile(config);
     }
     catch(const YAML::BadFile& e)
     {
         std::cerr << e.what() << '\n';
     }
-
-    // don't know how to generalize this
-    const std::string ip = config["network"]["ip"].as<std::string>();
-    const int port = config["network"]["port"].as<int>();
-    const int max_players = config["general"]["max_players"].as<int>();
-    const std::string motd = config["general"]["motd"].as<std::string>();
-
-    this->_configFile.insert({"ip", ip});
-    this->_configFile.insert({"port", std::to_string(port)});
-    this->_configFile.insert({"max_players", std::to_string(max_players)});
-    this->_configFile.insert({"motd", motd});
 }
 
-std::string Configuration::ConfigHandler::getNode(std::string node_name)
+YAML::Node Configuration::ConfigHandler::getNode(std::string node_name)
 {
-    std::string value;
-
     try
     {
-        if (this->_configFile.find(node_name) != this->_configFile.end()) {
-            value = this->_configFile.find(node_name)->second;
-            return value;
-        } else {
-            throw invalid_argument("This node does not exist.");
+        if (this->_configFile.count(node_name)) {
+            std::unordered_map<std::string, YAML::Node>::const_iterator got = this->_configFile.find(node_name);
+            if (got != this->_configFile.end()) {
+                return got->second;
+            } else {
+                throw std::invalid_argument("This node does not exist.");
+            }
         }
     }
     catch(const std::invalid_argument& e)
     {
         std::cerr << e.what() << '\n';
-        return nullptr;
     }
 }
