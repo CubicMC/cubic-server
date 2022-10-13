a18caf288c1973622771e839f6e1049c81a9c69f
NekoSelena <ode.marchand@epitech.eu>
2022-10-13 22:59:42 +0200

    Add logs and players display

--- interface/ManagementInterface.hpp
+++ interface/ManagementInterface.hpp
@@ -2,11 +2,19 @@
 #define MANAGEMENTINTERFACE_HPP
 
 #include <gtkmm/window.h>
+#include <gtkmm/paned.h>
+
+#include "LogsInterface.hpp"
+#include "PlayersInterface.hpp"
 
 class ManagementInterface : public Gtk::Window {
     public:
         ManagementInterface();
         virtual ~ManagementInterface();
+    protected:
+        Gtk::Paned m_content;
+        PlayersInterface m_players_section;
+        LogsInterface m_logs_section;
 };
 
 #endif /* !MANAGEMENTINTERFACE_HPP_ */
