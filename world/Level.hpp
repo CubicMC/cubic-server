032b08c9f86816d809447216eb4b5ae4664740dc
Trompettesib <alexandre.tissot@epitech.eu>
2022-11-17 19:20:07 +0100

    Fix addChunkColumn function and add an overload of this function

--- world/Level.hpp
+++ world/Level.hpp
@@ -1,12 +1,27 @@
 #include <unordered_map>
+#include <functional>
 
 #include "ChunkColumn.hpp"
 
-
-typedef struct _2d_pos {
+struct _2d_pos {
     int x;
     int z;
-} _2d_pos;
+
+    bool operator==(const _2d_pos& other) const {
+        return x == other.x && z == other.z;
+    }
+};
+
+template<>
+struct std::hash<_2d_pos>
+{
+    std::size_t operator()(_2d_pos const& pos) const noexcept
+    {
+        std::size_t h1 = std::hash<int>{}(pos.x);
+        std::size_t h2 = std::hash<int>{}(pos.z);
+        return h1 ^ (h2 << 1);
+    }
+};
 
 class Level
 {
@@ -14,13 +29,14 @@ public:
     Level();
     ~Level();
 
-    void addChunkColumn(_2d_pos pos, ChunkColumn& chunkColumn);
+    void addChunkColumn(_2d_pos pos, ChunkColumn &chunkColumn);
+    void addChunkColumn(_2d_pos pos);
     /** Get the chunk from chunk coordinate */
-    ChunkColumn& getChunkColumn(_2d_pos pos);
+    ChunkColumn &getChunkColumn(_2d_pos pos);
     /** Get the chunk from raw coordinate */
-    ChunkColumn& getChunkColumn(int x, int z);
+    ChunkColumn &getChunkColumn(int x, int z);
     void removeChunkColumn(_2d_pos pos);
 
 private:
-    std::unordered_map<_2d_pos, ChunkColumn&> _chunkColumns;
+    std::unordered_map<_2d_pos, ChunkColumn &> _chunkColumns;
 };
