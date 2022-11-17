e5ed75fc73b5f2de33b68e3fd662f7f558824bd7
Trompettesib <alexandre.tissot@epitech.eu>
2022-11-17 19:48:07 +0100

    Add the World's functions

--- world/World.cpp
+++ world/World.cpp
@@ -1,1 +1,41 @@
 #include "World.hpp"
+
+const LevelData &World::getLevelData() const
+{
+    return _levelData;
+}
+
+void World::setLevelData(const LevelData &value)
+{
+    _levelData = value;
+}
+
+const std::vector<std::string> World::getLevelNames() const
+{
+    std::vector<std::string> levelNames;
+    for (auto const &[key, val] : _levels) {
+        levelNames.push_back(key);
+    }
+    return levelNames;
+}
+
+const Level &World::getLevel(std::string &levelName) const
+{
+    return _levels.at(levelName);
+}
+
+void World::addLevel(std::string &levelName, Level &level)
+{
+    _levels.insert({levelName, level});
+}
+
+void World::addLevel(std::string &levelName)
+{
+    Level level;
+    _levels.insert({levelName, level});
+}
+
+void World::removeLevel(std::string &levelName)
+{
+    _levels.erase(levelName);
+}
--- world/World.hpp
+++ world/World.hpp
@@ -8,7 +8,17 @@ class World
 public:
     World();
     ~World();
+
+    const LevelData &getLevelData() const;
+    void setLevelData(const LevelData &value);
+
+    const std::vector<std::string> getLevelNames() const;
+    const Level &getLevel(std::string &levelName) const;
+    void addLevel(std::string &levelName, Level &level);
+    void addLevel(std::string &levelName);
+    void removeLevel(std::string &levelName);
+
 private:
     LevelData _levelData;
-    std::unordered_map<std::string, Level> _levels;
+    std::unordered_map<std::string, Level &> _levels;
 };
