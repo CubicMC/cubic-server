6a1e0e4ed98619790afe8fcc7f1db1c83821d235
Trompettesib <alexandre.tissot@epitech.eu>
2022-11-17 17:55:25 +0100

    Add the Level's functions

--- world/Level.cpp
+++ world/Level.cpp
@@ -1,1 +1,18 @@
 #include "Level.hpp"
+
+void Level::addChunkColumn(_2d_pos pos, ChunkColumn& chunkColumn) {
+    _chunkColumns.insert({pos, chunkColumn});
+}
+
+ChunkColumn &Level::getChunkColumn(_2d_pos pos) {
+    return _chunkColumns.at(pos);
+}
+
+ChunkColumn &Level::getChunkColumn(int x, int z) {
+    _2d_pos pos = {x < 0 ? -1 + int(x/16) : int(x/16), x < 0 ? -1 + int(z/16) : int(z/16)};
+    return _chunkColumns.at(pos);
+}
+
+void Level::removeChunkColumn(_2d_pos pos) {
+    _chunkColumns.erase(pos);
+}
--- world/Level.hpp
+++ world/Level.hpp
@@ -13,6 +13,14 @@ class Level
 public:
     Level();
     ~Level();
+
+    void addChunkColumn(_2d_pos pos, ChunkColumn& chunkColumn);
+    /** Get the chunk from chunk coordinate */
+    ChunkColumn& getChunkColumn(_2d_pos pos);
+    /** Get the chunk from raw coordinate */
+    ChunkColumn& getChunkColumn(int x, int z);
+    void removeChunkColumn(_2d_pos pos);
+
 private:
-    std::unordered_map<_2d_pos, ChunkColumn> _chunkColumns;
+    std::unordered_map<_2d_pos, ChunkColumn&> _chunkColumns;
 };
