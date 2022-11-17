0a7941568fd63dae2fefd60a961d06ea9b1840bf
Trompettesib <alexandre.tissot@epitech.eu>
2022-11-17 21:48:36 +0100

    Add test of LevelData class

--- world/tests/World_unittest.cpp
+++ world/tests/World_unittest.cpp
@@ -6,6 +6,10 @@ World world;
 TEST(WorldTest, addLevel_and_getLevelNames)
 {
     std::string levelName = "overworld";
+    ASSERT_EQ(world.getLevelNames().size(), 0);
+    world.addLevel(levelName);
+    ASSERT_EQ(world.getLevelNames().size(), 1);
+    ASSERT_EQ(world.getLevelNames()[0], levelName);
     world.addLevel(levelName);
     ASSERT_EQ(world.getLevelNames().size(), 1);
     ASSERT_EQ(world.getLevelNames()[0], levelName);
@@ -14,6 +18,10 @@ TEST(WorldTest, addLevel_and_getLevelNames)
 TEST(WorldTest, removeLevel)
 {
     std::string levelName = "overworld";
+    world.addLevel(levelName);
+    ASSERT_EQ(world.getLevelNames().size(), 1);
+    world.removeLevel(levelName);
+    ASSERT_EQ(world.getLevelNames().size(), 0);
     world.removeLevel(levelName);
     ASSERT_EQ(world.getLevelNames().size(), 0);
 }
