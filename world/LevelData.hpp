37a78eaac9ea36b1b23a78d620bce3c13ddf6b87
Trompettesib <alexandre.tissot@epitech.eu>
2022-11-17 18:08:19 +0100

    Add the LevelData's functions

--- world/LevelData.hpp
+++ world/LevelData.hpp
@@ -5,6 +5,59 @@ class LevelData
 public:
     LevelData();
     ~LevelData();
+
+    uint8_t getHardcore() const;
+    void setHardcore(uint8_t value);
+
+    uint8_t getMapFeatures() const;
+    void setMapFeatures(uint8_t value);
+
+    uint8_t getRaining() const;
+    void setRaining(uint8_t value);
+
+    uint8_t getThundering() const;
+    void setThundering(uint8_t value);
+
+    uint32_t getGameType() const;
+    void setGameType(uint32_t value);
+
+    uint32_t getGeneratorVersion() const;
+    void setGeneratorVersion(uint32_t value);
+
+    uint32_t getRainTime() const;
+    void setRainTime(uint32_t value);
+
+    uint32_t getSpawnX() const;
+    void setSpawnX(uint32_t value);
+    uint32_t getSpawnY() const;
+    void setSpawnY(uint32_t value);
+    uint32_t getSpawnZ() const;
+    void setSpawnZ(uint32_t value);
+
+    uint32_t getThunderTime() const;
+    void setThunderTime(uint32_t value);
+
+    uint32_t getVersion() const;
+    void setVersion(uint32_t value);
+
+    uint64_t getLastPlayed() const;
+    void setLastPlayed(uint64_t value);
+
+    uint64_t getSeed() const;
+    void setSeed(uint64_t value);
+
+    uint64_t getSizeOnDisk() const;
+    void setSizeOnDisk(uint64_t value);
+
+    uint64_t getTime() const;
+    void setTime(uint64_t value);
+
+    const std::string& getGeneratorName() const;
+    void setGeneratorName(const std::string &value);
+
+    const std::string& getLevelName() const;
+    void setLevelName(const std::string &value);
+
 private:
     uint8_t _hardcore;
     uint8_t _mapFeatures;
