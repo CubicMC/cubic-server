02ea801c31ab1baab448b44605a949d51802edc1
Michaël Veillet Guillem <micou.veillet@gmail.com>
2022-11-18 11:02:49 +0100

    Using serialize variadic argument call instead of calling it multiple times

--- protocol/ClientPackets.cpp
+++ protocol/ClientPackets.cpp
@@ -26,18 +26,18 @@ std::shared_ptr<std::vector<uint8_t>> protocol::createStatusResponse(const Statu
 std::shared_ptr<std::vector<uint8_t>> protocol::createPlayerChatMessage(const PlayerChatMessage &in)
 {
     std::vector<uint8_t> payload;
-    serialize(payload, in.signedContent, addString);
-    serialize(payload, in.hasUnsignedContent, addBoolean);
-    serialize(payload, in.unsignedContent, addString);
-    serialize(payload, in.type, addVarInt);
-    serialize(payload, in.senderUUID, addUUID);
-    serialize(payload, in.senderName, addString);
-    serialize(payload, in.hasTeamName, addBoolean);
-    serialize(payload, in.teamName, addString);
-    serialize(payload, in.timestamp, addLong);
-    serialize(payload, in.salt, addLong);
-    serialize(payload, in.signatureLength, addVarInt);
-    serialize(payload, in.signature, addByteArray);
+    serialize(payload, in.signedContent, addString,
+        in.hasUnsignedContent, addBoolean,
+        in.unsignedContent, addString,
+        in.type, addVarInt,
+        in.senderUUID, addUUID,
+        in.senderName, addString,
+        in.hasTeamName, addBoolean,
+        in.teamName, addString,
+        in.timestamp, addLong,
+        in.salt, addLong,
+        in.signatureLength, addVarInt,
+        in.signature, addByteArray);
     auto packet = std::make_shared<std::vector<uint8_t>>();
     finalize(*packet.get(), payload, (int32_t) ClientPacketID::PlayerChatMessage);
     return packet;
