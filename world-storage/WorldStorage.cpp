5d38b8935532b50eea81e4a81b9ffd4021f7b306
Trompettesib <alexandre.tissot@epitech.eu>
2022-11-18 11:28:43 +0100

    Rename of world class and folder to WorldStorage

---
+++ world-storage/WorldStorage.cpp
@@ -0,0 +1,49 @@
+#include "WorldStorage.hpp"
+
+WorldStorage::WorldStorage()
+{
+}
+
+WorldStorage::~WorldStorage()
+{
+}
+
+const LevelData &WorldStorage::getLevelData() const
+{
+    return _levelData;
+}
+
+void WorldStorage::setLevelData(const LevelData &value)
+{
+    _levelData = value;
+}
+
+const std::vector<std::string> WorldStorage::getLevelNames() const
+{
+    std::vector<std::string> levelNames;
+    for (auto const &[key, val] : _levels) {
+        levelNames.push_back(key);
+    }
+    return levelNames;
+}
+
+const Level &WorldStorage::getLevel(std::string &levelName) const
+{
+    return _levels.at(levelName);
+}
+
+void WorldStorage::addLevel(std::string &levelName, Level &level)
+{
+    _levels.insert({levelName, level});
+}
+
+void WorldStorage::addLevel(std::string &levelName)
+{
+    Level level;
+    _levels.insert({levelName, level});
+}
+
+void WorldStorage::removeLevel(std::string &levelName)
+{
+    _levels.erase(levelName);
+}
