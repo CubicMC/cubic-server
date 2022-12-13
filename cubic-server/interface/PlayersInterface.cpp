c105ee9b8d73882ea42ee10653cb75f72cd32c99
Ode Marchand <62648027+NekoSelena@users.noreply.github.com>
2022-12-13 23:34:18 +0100

    Add textbox + event handler (key not bind)

--- cubic-server/interface/PlayersInterface.cpp
+++ cubic-server/interface/PlayersInterface.cpp
@@ -9,15 +9,21 @@ PlayersInterface::PlayersInterface()
 {
     set_shadow_type(Gtk::SHADOW_ETCHED_OUT);
     set_policy(Gtk::POLICY_AUTOMATIC, Gtk::POLICY_AUTOMATIC);
-    add(this->m_label);
 
+    this->m_label.set_justify(Gtk::JUSTIFY_LEFT);
     this->m_label.set_xalign(0);
     this->m_label.set_yalign(0);
     this->m_label.set_text("GUI initialisation...");
+    add(this->m_label);
 
-    int timeout_value = 100; //in ms
-    sigc::slot<bool>my_slot = sigc::mem_fun(*this, &PlayersInterface::on_timeout);
-    Glib::signal_timeout().connect(my_slot, timeout_value);
+    ///////////////////////////////////////
+    // Not working anymore, for some reason
+
+    // int timeout_value = 100; //in ms
+    // sigc::slot<bool>my_slot = sigc::mem_fun(*this, &PlayersInterface::on_timeout);
+    // Glib::signal_timeout().connect(my_slot, timeout_value);
+
+    ///////////////////////////////////////
 
     show_all_children();
 }
