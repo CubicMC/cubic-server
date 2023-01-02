6d3fd41994797eacd53382ab604ceef47f2a7472
Trompettesib <alexandre.tissot@epitech.eu>
2023-01-02 20:32:35 +0100

    Add command parsing that compile when not used

---
+++ cubic-server/command-parser/CommandParser.hpp
@@ -0,0 +1,18 @@
+#ifndef CUBICSERVER_COMMANDPARSER_HPP
+#define CUBICSERVER_COMMANDPARSER_HPP
+
+#include <string>
+#include <unordered_map>
+#include <functional>
+
+#include "commands/help.hpp"
+
+namespace CommandParser {
+    const std::unordered_map<std::string, std::function<void(std::vector<std::string>&)>> commands {
+        {"/help", help::execute},
+        {"/?", help::execute}
+    };
+    void parseCommand(std::string &command);
+}
+
+#endif //CUBICSERVER_COMMANDPARSER_HPP
