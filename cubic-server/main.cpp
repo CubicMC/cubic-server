745802f4d1e9bbd7dc60c9c98cea628da593aef3
Ode Marchand <62648027+NekoSelena@users.noreply.github.com>
2023-01-03 23:51:52 +0100

    Update arg parser with argparse lib

--- cubic-server/main.cpp
+++ cubic-server/main.cpp
@@ -3,52 +3,38 @@
 #include <unordered_map>
 #include <functional>
 
+#include <argparse/argparse.hpp>
 #include "Server.hpp"
 #include "protocol/ServerPackets.hpp"
 #include "interface/ManagementInterface.hpp"
 
-bool gui = true;
+bool arg_parser(int argc, char **argv) {
+    argparse::ArgumentParser program("cubic_server");
 
-int print_usage(const char *caller)
-{
-    std::cout << "Usage:\n\t" << caller << " [FLAGS]"<< std::endl;
-    std::cout << "\t\t-h:\t\tDisplays this help." << std::endl;
-    std::cout << "\t\t--help:\t\tDisplays this help." << std::endl;
-    std::cout << "\t\t--nogui:\tPrevents the GUI from displaying." << std::endl;
-    return 1;
-}
-
-int hide_gui(const char *no) {
-    gui = false;
-    return 2;
-}
-
-int arg_parser(int argc, char **argv) {
-    std::unordered_map<std::string, std::function<int(const char *)>> args;
-    int res = 0;
-
-    args["-h"] = std::function<int(const char *)>(print_usage);
-    args["--help"] = std::function<int(const char *)>(print_usage);
-    args["--nogui"] = std::function<int(const char *)>(hide_gui);
-
-    for(int i = 1; i < argc; i++) {
-        std::string flag = argv[i];
-        res = std::any_cast<std::function<int(const char *)>>(args[flag])(argv[0]);
-        if (res == 1) {
-            return res;
-        }
+    program.add_argument("--nogui")
+        .help("prevents the GUI from displaying")
+        .default_value(true)
+        .implicit_value(false);
+    try {
+        program.parse_args(argc, argv);
     }
-    return res;
+    catch (const std::runtime_error& err) {
+        std::cerr << err.what() << std::endl;
+        std::cerr << program;
+        std::exit(1);
+    }
+
+    if (program["--nogui"] == false) {
+        return false;
+    }
+    return true;
+
 }
 
 int main(int argc, char **argv)
 {
-    int check_args = arg_parser(argc, argv);
+    bool gui = arg_parser(argc, argv);
     std::thread InterfaceThread;
-
-    if (check_args == 1) {
-        return 0;
-    }
 
     if (gui) {
         InterfaceThread = std::thread(&ManagementInterface::launch, argc, argv);
