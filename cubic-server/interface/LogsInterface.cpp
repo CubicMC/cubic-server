0dcd8af9b5763fac2b97a8c9af2819fcba275b84
Ode Marchand <62648027+NekoSelena@users.noreply.github.com>
2023-01-12 23:26:32 +0100

    Update interface
    - Add filter buttons
    - Replace label with TextView ( fix issue #68 )

--- cubic-server/interface/LogsInterface.cpp
+++ cubic-server/interface/LogsInterface.cpp
@@ -8,17 +8,37 @@
 #include "LogsInterface.hpp"
 
 LogsInterface::LogsInterface() :
-  m_VBox(Gtk::ORIENTATION_VERTICAL)
+  m_VBox_main(Gtk::ORIENTATION_VERTICAL),
+  m_VBox_logs(Gtk::ORIENTATION_VERTICAL),
+  m_HBox_filters(Gtk::ORIENTATION_HORIZONTAL),
+  m_Info("Info"),
+  m_Warn("Warning"),
+  m_Error("Error"),
+  m_Fatal("Fatal"),
+  m_Debug("Debug")
 {
-    set_shadow_type(Gtk::SHADOW_ETCHED_OUT);
+    set_shadow_type(Gtk::SHADOW_IN);
     set_policy(Gtk::POLICY_AUTOMATIC, Gtk::POLICY_AUTOMATIC);
-    add(m_VBox);
+    add(m_VBox_main);
 
-    m_VBox.pack_start(m_label);
-    m_label.set_xalign(0);
-    m_label.set_yalign(0);
-    m_label.set_line_wrap(true);
-    m_label.set_text("GUI initialisation...");
+    m_VBox_main.pack_start(m_VBox_logs);
+    m_VBox_logs.pack_start(m_Frame_filters, Gtk::PACK_SHRINK);
+    m_Frame_filters.add(m_HBox_filters);
+
+    m_HBox_filters.pack_start(m_Info);
+    m_HBox_filters.pack_start(m_Warn);
+    m_HBox_filters.pack_start(m_Error);
+    m_HBox_filters.pack_start(m_Fatal);
+    m_HBox_filters.pack_start(m_Debug);
+    m_HBox_filters.set_border_width(10);
+    m_HBox_filters.set_layout(Gtk::BUTTONBOX_SPREAD);
+
+    m_VBox_logs.pack_end(m_Logs_view);
+
+    m_Logs_view.set_wrap_mode(Gtk::WRAP_CHAR);
+    m_Logs = Gtk::TextBuffer::create();
+    m_Logs->set_text("GUI initialisation...");
+    m_Logs_view.set_buffer(m_Logs);
 
     m_Entry.set_max_length(50);
     m_Entry.set_valign(Gtk::ALIGN_END);
@@ -29,7 +49,7 @@ LogsInterface::LogsInterface() :
     // at all. /!\ To do when handling command parsing.
     m_Entry.set_activates_default(true);
 
-    m_VBox.pack_end(m_Entry);
+    m_VBox_main.pack_end(m_Entry);
 
     int timeout_value = 100; //in ms
     sigc::slot<bool>my_slot = sigc::mem_fun(*this, &LogsInterface::on_timeout);
@@ -44,7 +64,7 @@ bool LogsInterface::on_key_press_event(GdkEventKey* event)
     {
         // Supposed to add the input to the logs (interface side only) when 'ENTER'
         // is pressed. /!\ To do when handling command parsing.
-        m_label.set_text(m_label.get_text() + "\n" + m_Entry.get_text());
+        m_Logs->set_text(m_Logs->get_text() + "\n" + m_Entry.get_text());
         m_Entry.set_text("");
         return true;
     }
@@ -52,7 +72,7 @@ bool LogsInterface::on_key_press_event(GdkEventKey* event)
     if(event->keyval == GDK_KEY_Escape)
     {
         // Add the input to the logs (interface side only) when 'ESC' pressed
-        m_label.set_text(m_label.get_text() + "\n" + m_Entry.get_text());
+        m_Logs->set_text(m_Logs->get_text() + "\n" + m_Entry.get_text());
         m_Entry.set_text("");
         return true;
     }
@@ -60,7 +80,8 @@ bool LogsInterface::on_key_press_event(GdkEventKey* event)
     return false;
 }
 
-bool LogsInterface::on_timeout(){
+bool LogsInterface::on_timeout()
+{
     std::string temp = "";
     std::stringstream ss;
     std::queue<logging::LogMessage> q_copy = logging::Logger::get_instance()->get_logs();
@@ -72,9 +93,14 @@ bool LogsInterface::on_timeout(){
         temp = ss.str();
         q_copy.pop();
     }
-    m_label.set_text(temp.c_str());
+    m_Logs->set_text(temp.c_str());
 
  return true;
+}
+
+void LogsInterface::on_button_clicked()
+{
+    return;
 }
 
 LogsInterface::~LogsInterface()
--- cubic-server/interface/LogsInterface.hpp
+++ cubic-server/interface/LogsInterface.hpp
@@ -7,15 +7,20 @@ class LogsInterface : public Gtk::ScrolledWindow {
     public:
         LogsInterface();
         virtual ~LogsInterface();
-        bool on_timeout();
 
     private:
+        bool on_timeout();
         bool on_key_press_event(GdkEventKey* event) override;
+        void on_button_clicked();
 
     protected:
-        Gtk::Label m_label;
+        Gtk::TextView m_Logs_view;
+        Glib::RefPtr<Gtk::TextBuffer> m_Logs;
         Gtk::Entry m_Entry;
-        Gtk::Box m_VBox;
+        Gtk::Box m_VBox_main, m_VBox_logs;
+        Gtk::Frame m_Frame_filters;
+        Gtk::ButtonBox m_HBox_filters;
+        Gtk::Button m_Info, m_Warn, m_Error, m_Fatal, m_Debug;
 };
 
 #endif /* !LOGSINTERFACE_HPP */
