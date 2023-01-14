915136cf077dc94f965b321f6d38b1248fc5a618
Michaël Veillet Guillem <micou.veillet@gmail.com>
2023-01-14 22:13:32 +0100

    Fix typo

--- cubic-server/Player.hpp
+++ cubic-server/Player.hpp
@@ -17,7 +17,7 @@ public:
     Player(Client *cli, std::shared_ptr<Dimension> dim);
     void tick() override;
     Client *getClient() const;
-    void disconnect(const chat::Message &reason = chat::Message("Disconected"));
+    void disconnect(const chat::Message &reason = chat::Message("Disconnected"));
     void playSoundEffect(SoundsList sound, protocol::FloatingPosition position, SoundCategory category = SoundCategory::Master);
     void playSoundEffect(SoundsList sound, const Entity *entity, SoundCategory category = SoundCategory::Master);
     void playCustomSound(std::string sound, protocol::FloatingPosition position, SoundCategory category = SoundCategory::Master);
