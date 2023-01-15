9c266d0f58124db02cbbecfcc06c145c8aad7197
Alexandre Flion <dev@huntears.com>
2023-01-15 12:17:33 +0100

    Make the server not commit sepuku every tick

--- cubic-server/default/DefaultWorld.cpp
+++ cubic-server/default/DefaultWorld.cpp
@@ -6,14 +6,21 @@ DefaultWorld::DefaultWorld(WorldGroup *worldGroup)
     this->_dimensions.emplace("end", std::make_shared<TheEnd>(reinterpret_cast<World *>(this)));
     this->_dimensions.emplace("nether", std::make_shared<TheNether>(reinterpret_cast<World *>(this)));
     this->_dimensions.emplace("overworld", std::make_shared<Overworld>(reinterpret_cast<World *>(this)));
+    for (auto [_, dim] : this->_dimensions)
+        _processingThreads.push_back(new std::thread([dim](){
+            while (1) {
+                dim->dimensionLock.acquire();
+                dim->tick();
+            }
+        }));
 }
 
 void DefaultWorld::tick()
 {
     World::tick();
     // TODO: Launch threads for all the dimensions
-    for (auto &dim : this->_dimensions)
-        _processingThreads.push_back(new std::thread(&Dimension::tick, dim.second));
+    for (auto [_, dim] : this->_dimensions)
+        dim->dimensionLock.release();
 }
 
 void DefaultWorld::initialize()
