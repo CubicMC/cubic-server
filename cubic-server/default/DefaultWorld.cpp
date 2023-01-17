d5cc6afc88e0cfd2bf0a9c6c4e1cdef083fb214f
Thomas Harit <thomas.harit@epitech.eu>
2023-01-18 04:12:59 +0900

    Fixed saved level.dat file compression algorithm

--- cubic-server/default/DefaultWorld.cpp
+++ cubic-server/default/DefaultWorld.cpp
@@ -3,7 +3,7 @@
 #include <zlib.h>
 #include <iostream>
 
-#define CHUNK 0x4000
+#define CHUNK 0xFF
 
 #include "Overworld.hpp"
 #include "TheEnd.hpp"
@@ -30,7 +30,7 @@ void DefaultWorld::initialize()
 
 // TODO: When _data will be initialized don't forget to remove this phony data and put the function to const !
 void DefaultWorld::save() {
-    this->_data = std::make_shared<nbt::Compound>("Data", std::initializer_list<nbt::Base *>{
+    nbt::Compound *_data = new nbt::Compound("Data", {
             new nbt::Byte("allowCommands", 0),
             new nbt::Double("BorderCenterX", 0),
             new nbt::Double("BorderCenterZ", 0),
@@ -43,21 +43,21 @@ void DefaultWorld::save() {
             new nbt::Double("BorderWarningBlocks", 5),
             new nbt::Double("BorderWarningTime", 15),
             new nbt::Int("ClearWeatherTime", 0),
-            new nbt::Compound("CustomBossEvents", std::initializer_list<nbt::Base *>{}),
-            new nbt::Compound("DataPacks", std::initializer_list<nbt::Base *>{}),
+            new nbt::Compound("CustomBossEvents", {}),
+            new nbt::Compound("DataPacks", {}),
             new nbt::Int("DataVersion", 3120),
             new nbt::Long("DayTime", 0),
             new nbt::Byte("Difficulty", 0),
             new nbt::Byte("DifficultyLocked", 0),
-            new nbt::Compound("DimensionData", std::initializer_list<nbt::Base *>{
-                    new nbt::Compound("1", std::initializer_list<nbt::Base *>{
-                            new nbt::Compound("DragonFight", std::initializer_list<nbt::Base *>{
-                                    new nbt::Compound("ExitPortalLocation", std::initializer_list<nbt::Base *>{
+            new nbt::Compound("DimensionData", {
+                    new nbt::Compound("1", {
+                            new nbt::Compound("DragonFight", {
+                                    new nbt::Compound("ExitPortalLocation", {
                                             new nbt::Byte("X", 0),
                                             new nbt::Byte("Y", 0),
                                             new nbt::Byte("Z", 0),
                                     }),
-                                    new nbt::List("Gateways"),
+                                    new nbt::List("Gateways", {}),
                                     new nbt::Byte("DragonKilled", 0),
                                     new nbt::Long("DragonUUIDLeast", 0),
                                     new nbt::Long("DragonUUIDMost", 0),
@@ -65,14 +65,14 @@ void DefaultWorld::save() {
                             })
                     })
             }),
-            new nbt::Compound("GameRules", std::initializer_list<nbt::Base *>{}),
+            new nbt::Compound("GameRules", {}),
             new nbt::Int("GameType", 0),
             new nbt::Byte("hardcore", 0),
             new nbt::Byte("initialized", 0),
             new nbt::Long("LastPlayed", 0),
             new nbt::String("LevelName", "World"),
             new nbt::Byte("MapFeatures", 0),
-            new nbt::Compound("Player", std::initializer_list<nbt::Base *>{}),
+            new nbt::Compound("Player", {}),
             new nbt::Byte("raining", 0),
             new nbt::Int("rainTime", 0),
             new nbt::Long("RandomSeed", 0),
@@ -84,7 +84,7 @@ void DefaultWorld::save() {
             new nbt::Int("thunderTime", 0),
             new nbt::Long("Time", 0),
             new nbt::Int("Time", 19133 ),
-            new nbt::Compound("Version", std::initializer_list<nbt::Base *>{
+            new nbt::Compound("Version", {
                     new nbt::Int("Id", 3120),
                     new nbt::String("Name", "1.19.2"),
                     new nbt::String("Series", "main"),
@@ -99,9 +99,7 @@ void DefaultWorld::save() {
 
     FILE *file_buf = fopen("level.dat", "w");
     unsigned char *in = buffer.data();
-    unsigned long in_len = buffer.size();
-    unsigned char out[CHUNK + 1];
-    unsigned long out_len = CHUNK;
+    unsigned char out[CHUNK];
     int res;
 
     z_stream strm;
@@ -110,23 +108,20 @@ void DefaultWorld::save() {
     strm.zfree = Z_NULL;
     strm.opaque = Z_NULL;
 
-    deflateInit(&strm, Z_DEFAULT_COMPRESSION);
+    deflateInit2(&strm, Z_DEFAULT_COMPRESSION, Z_DEFLATED, 15 | 16, 8, Z_DEFAULT_STRATEGY);
 
     strm.next_in = in;
-    strm.avail_in = in_len;
-    strm.next_out = 0;
-    strm.avail_out = 0;
+    strm.avail_in = buffer.size();
 
     do {
-        if (strm.avail_out == 0) {
-            strm.next_out = out;
-            strm.avail_out = CHUNK;
-        }
-        res = deflate(&strm, in_len - strm.total_out > CHUNK ? Z_NO_FLUSH : Z_FINISH);
-    } while (res == Z_OK);
+        strm.avail_out = CHUNK;
+        strm.next_out = out;
+        deflate(&strm, Z_FINISH);
+        fwrite(out, sizeof(char), CHUNK - strm.avail_out, file_buf);
+    } while (strm.avail_out == 0);
 
-    fwrite(out, sizeof(char), out_len, file_buf);
     fclose(file_buf);
+    deflateEnd(&strm);
 }
 
 void DefaultWorld::stop() { World::stop(); }
