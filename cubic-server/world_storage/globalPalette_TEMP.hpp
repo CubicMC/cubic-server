ae518e2589bf3560744de8e5297228232cdfd905
Alexandre Flion <dev@huntears.com>
2023-02-08 00:18:03 +0100

    Update protocol IDs to 1.19.3

--- cubic-server/world_storage/globalPalette_TEMP.hpp
+++ cubic-server/world_storage/globalPalette_TEMP.hpp
@@ -12,19 +12,16 @@ namespace world_storage
 constexpr GlobalBlockId getGlobalPaletteIdFromBlockName(const std::string &name)
 {
     if (name == "minecraft:air") return 0;
-    else if (name == "minecraft:bedrock") return 74;
+    else if (name == "minecraft:bedrock") return 76;
     else if (name == "minecraft:grass_block") return 9;
-    else if (name == "minecraft:grass") return 1596;
-    else if (name == "minecraft:stone") return 1;
-    else if (name == "minecraft:water") return 75;
-    else if (name == "minecraft:sand") return 107;
+    else if (name == "minecraft:grass") return 1954;
     else if (name == "minecraft:dirt") return 10;
-    else if (name == "minecraft:oak_log") return 118;
-    else if (name == "minecraft:oak_leaves") return 207;
-    else if (name == "minecraft:glass") return 460;
+    else if (name == "minecraft:oak_log") return 120;
+    else if (name == "minecraft:oak_leaves") return 241;
+    else if (name == "minecraft:glass") return 468;
     else if (name == "minecraft:cobblestone") return 14;
-    else if (name == "minecraft:pink_terracotta") return 7482;
-    else if (name == "minecraft:purple_carpet") return 8617;
+    else if (name == "minecraft:pink_terracotta") return 8966;
+    else if (name == "minecraft:purple_carpet") return 10261;
 
     // Default
     return 0;
@@ -35,16 +32,16 @@ GlobalBlockId getGlobalPaletteIdFromBlock(const Block &block);
 constexpr std::string getBlockFromGlobalPaletteId(GlobalBlockId id)
 {
     if (id == 0) return "minecraft:air";
-    else if (id == 74) return "minecraft:bedrock";
+    else if (id == 76) return "minecraft:bedrock";
     else if (id == 9) return "minecraft:grass_block";
-    else if (id == 1596) return "minecraft:grass";
+    else if (id == 1954) return "minecraft:grass";
     else if (id == 10) return "minecraft:dirt";
-    else if (id == 118) return "minecraft:oak_log";
-    else if (id == 207) return "minecraft:oak_leaves";
-    else if (id == 460) return "minecraft:glass";
+    else if (id == 120) return "minecraft:oak_log";
+    else if (id == 241) return "minecraft:oak_leaves";
+    else if (id == 468) return "minecraft:glass";
     else if (id == 14) return "minecraft:cobblestone";
-    else if (id == 7482) return "minecraft:pink_terracotta";
-    else if (id == 8617) return "minecraft:purple_carpet";
+    else if (id == 8966) return "minecraft:pink_terracotta";
+    else if (id == 10261) return "minecraft:purple_carpet";
 
     // Default
     return "minecraft:air";
