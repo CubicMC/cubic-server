e47e01f872a3235ca542059b93578aaba1e970f7
Alexandre Flion <huntears@kreog.com>
2023-04-16 00:27:05 +0200

    Remove can't keep up, since we can't calculate it properly

--- cubic-server/WorldGroup.cpp
+++ cubic-server/WorldGroup.cpp
@@ -36,14 +36,6 @@ void WorldGroup::_run()
         _soundSystem->tick();
         auto end_time = std::chrono::system_clock::now();
         auto compute_time = end_time - start_time;
-        if (compute_time > std::chrono::milliseconds(MS_PER_TICK)) { // If this happens there is a serious problem
-            auto nb_ticks = compute_time / std::chrono::milliseconds(MS_PER_TICK);
-            LWARN(
-                "Can't keep up! Did the system time change, or is the server overloaded? Running ", std::chrono::duration_cast<std::chrono::milliseconds>(compute_time).count(),
-                "ms behind, skipping ", nb_ticks, " tick(s)"
-            );
-            continue;
-        }
         std::this_thread::sleep_for(std::chrono::milliseconds(MS_PER_TICK) - compute_time);
     }
 }
