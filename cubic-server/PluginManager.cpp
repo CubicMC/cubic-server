0c5885fafcd0947da4a137b871c4708f8fd5cddc
Thomas Harit <thomas.harit@epitech.eu>
2023-04-26 00:02:13 +0900

    Added PluginManager (not tested with plugin yet)

--- cubic-server/PluginManager.cpp
+++ cubic-server/PluginManager.cpp
@@ -1,23 +1,70 @@
-
-
 #include "PluginManager.hpp"
 
-PluginManager::PluginManager(std::string &folder) : folder(folder)
+#include <dlfcn.h>
+#include <fstream>
+#include <filesystem>
+#include <array>
+
+PluginManager::PluginManager(std::string &folder) : _folder(folder)
 {
 
 }
 
-PluginManager::~PluginManager()
+void PluginManager::loadPlugin(std::string filepath)
 {
+    const std::array<const char *, 21> EventKeyArray = {
+        EventKey::initialize,
+        EventKey::destroy,
+        EventKey::onPlayerJoin,
+        EventKey::onPlayerLeave,
+        EventKey::onPlayerChat,
+        EventKey::onEntitySpawn,
+        EventKey::onEntityMove,
+        EventKey::onEntityRotate,
+        EventKey::onEntityInteractEntity,
+        EventKey::onEntityInteractBlock,
+        EventKey::onEntityDamage,
+        EventKey::onEntityUse,
+        EventKey::onBlockPlace,
+        EventKey::onBlockDestroy,
+        EventKey::onBlockInteract,
+        EventKey::onInventoryOpen,
+        EventKey::onInventoryClose,
+        EventKey::onInventoryChange,
+        EventKey::onWorldLoad,
+        EventKey::onDimensionLoad,
+        EventKey::onChunkLoad,
+    };
+    void *nhandle = nullptr;
+    void *rawptr = nullptr;
 
+    nhandle = dlopen(filepath.c_str(), RTLD_LAZY);
+    if (!nhandle) {
+        //SRO
+    }
+    for (const auto &key : EventKeyArray) {
+        rawptr = dlsym(nhandle, key);
+        if (rawptr) {
+            this->_events[key].push_back(rawptr);
+        }
+    }
+    dlclose(nhandle);
 }
 
-void load(void)
+void PluginManager::load(void)
 {
+    if (!std::filesystem::is_directory(this->_folder))
+        return;
 
+    for (const auto &filepath : std::filesystem::directory_iterator(_folder)) {
+        if (filepath.path().string().ends_with(".so")) {
+            loadPlugin(filepath.path().string());
+        }
+    }
 }
 
-void reload(void)
+void PluginManager::reload(void)
 {
-
+    this->_events.clear();
+    this->load();
 }
--- cubic-server/PluginManager.hpp
+++ cubic-server/PluginManager.hpp
@@ -4,8 +4,8 @@
 #include <string>
 #include <vector>
 #include <unordered_map>
-#include <functional>
 #include <variant>
+#include <memory>
 
 class PluginInterface;
 
@@ -22,68 +22,102 @@ class Chunk;
 
 class Vector3;
 
-namespace PluginType {
-    using initialize = std::function<void(PluginInterface *interface)>;
-    using destroy = std::function<void(PluginInterface *interface)>;
-    using onPlayerJoin = std::function<void(PluginInterface *interface, Player *player)>;
-    using onPlayerLeave = std::function<void(PluginInterface *interface, Player *player)>;
-    using onPlayerChat = std::function<void(PluginInterface *interface, Player *player, Chat *chat, std::string message)>;
-    using onEntitySpawn = std::function<void(PluginInterface *interface, Entity *entity)>;
-    using onEntityMove = std::function<void(PluginInterface *interface, Entity *entity, Vector3 *from, Vector3 *to)>;
-    using onEntityRotate = std::function<void(PluginInterface *interface, Entity *entity, Vector3 *from, Vector3 *to)>;
-    using onEntityInteractEntity = std::function<void(PluginInterface *interface, Entity *source, Entity *target)>;
-    using onEntityInteractBlock = std::function<void(PluginInterface *interface, Entity *entity, Block *block)>;
-    using onEntityDamage = std::function<void(PluginInterface *interface, Entity *source, float amount)>;
-    using onEntityUse = std::function<void(PluginInterface *interface, Entity *entity, Item *item)>;
-    using onBlockPlace = std::function<void(PluginInterface *interface, Block *block, Vector3 *position)>;
-    using onBlockDestroy = std::function<void(PluginInterface *interface, Block *block, Vector3 *position)>;
-    using onBlockInteract = std::function<void(PluginInterface *interface, Block *block, Vector3 *position, Entity *entity)>;
-    using onInventoryOpen = std::function<void(PluginInterface *interface, Player *player, Inventory *inventory)>;
-    using onInventoryClose = std::function<void(PluginInterface *interface, Player *player, Inventory *inventory)>;
-    using onInventoryChange = std::function<void(PluginInterface *interface, Inventory *inventory)>;
-    using onWorldLoad = std::function<void(PluginInterface *interface, World *world)>;
-    using onDimensionLoad = std::function<void(PluginInterface *interface, Dimension *dimension)>;
-    using onChunkLoad = std::function<void(PluginInterface *interface, Chunk *chunk)>;
+namespace EventType {
+    typedef void (*initialize)(PluginInterface *interface);
+    typedef void (*destroy)(PluginInterface *interface);
+    typedef void (*onPlayerJoin)(PluginInterface *interface, Player *player);
+    typedef void (*onPlayerLeave)(PluginInterface *interface, Player *player);
+    typedef void (*onPlayerChat)(PluginInterface *interface, Player *player, Chat *chat, std::string message);
+    typedef void (*onEntitySpawn)(PluginInterface *interface, Entity *entity);
+    typedef void (*onEntityMove)(PluginInterface *interface, Entity *entity, Vector3 *from, Vector3 *to);
+    typedef void (*onEntityRotate)(PluginInterface *interface, Entity *entity, Vector3 *from, Vector3 *to);
+    typedef void (*onEntityInteractEntity)(PluginInterface *interface, Entity *source, Entity *target);
+    typedef void (*onEntityInteractBlock)(PluginInterface *interface, Entity *entity, Block *block);
+    typedef void (*onEntityDamage)(PluginInterface *interface, Entity *source, float amount);
+    typedef void (*onEntityUse)(PluginInterface *interface, Entity *entity, Item *item);
+    typedef void (*onBlockPlace)(PluginInterface *interface, Block *block, Vector3 *position);
+    typedef void (*onBlockDestroy)(PluginInterface *interface, Block *block, Vector3 *position);
+    typedef void (*onBlockInteract)(PluginInterface *interface, Block *block, Vector3 *position, Entity *entity);
+    typedef void (*onInventoryOpen)(PluginInterface *interface, Player *player, Inventory *inventory);
+    typedef void (*onInventoryClose)(PluginInterface *interface, Player *player, Inventory *inventory);
+    typedef void (*onInventoryChange)(PluginInterface *interface, Inventory *inventory);
+    typedef void (*onWorldLoad)(PluginInterface *interface, World *world);
+    typedef void (*onDimensionLoad)(PluginInterface *interface, Dimension *dimension);
+    typedef void (*onChunkLoad)(PluginInterface *interface, Chunk *chunk);
 
-    using EventTypeVariant = std::variant<
-        initialize, 
-        destroy, 
-        onPlayerJoin, 
-        onPlayerLeave, 
-        onPlayerChat, 
-        onEntitySpawn, 
-        onEntityMove, 
-        onEntityRotate, 
-        onEntityInteractEntity, 
-        onEntityInteractBlock, 
-        onEntityDamage, 
-        onEntityUse, 
-        onBlockPlace, 
-        onBlockDestroy, 
-        onBlockInteract, 
-        onInventoryOpen, 
-        onInventoryClose, 
-        onInventoryChange, 
-        onWorldLoad, 
-        onDimensionLoad, 
-        onChunkLoad
-    >;
+    union AllTypes {
+        void *rawptr;
+        initialize initialize_type;
+        destroy destroy_type;
+        onPlayerJoin onPlayerJoin_type;
+        onPlayerLeave onPlayerLeave_type;
+        onPlayerChat onPlayerChat_type;
+        onEntitySpawn onEntitySpawn_type;
+        onEntityMove onEntityMove_type;
+        onEntityRotate onEntityRotate_type;
+        onEntityInteractEntity onEntityInteractEntity_type;
+        onEntityInteractBlock onEntityInteractBlock_type;
+        onEntityDamage onEntityDamage_type;
+        onEntityUse onEntityUse_type;
+        onBlockPlace onBlockPlace_type;
+        onBlockDestroy onBlockDestroy_type;
+        onBlockInteract onBlockInteract_type;
+        onInventoryOpen onInventoryOpen_type;
+        onInventoryClose onInventoryClose_type;
+        onInventoryChange onInventoryChange_type;
+        onWorldLoad onWorldLoad_type;
+        onDimensionLoad onDimensionLoad_type;
+        onChunkLoad onChunkLoad_type;
+
+        AllTypes(void *rawptr) : rawptr(rawptr) {};
+        ~AllTypes() {};
+    };
 };
 
+namespace EventKey {
+    const char *initialize = "initialize";
+    const char *destroy = "destroy";
+    const char *onPlayerJoin = "onPlayerJoin";
+    const char *onPlayerLeave = "onPlayerLeave";
+    const char *onPlayerChat = "onPlayerChat";
+    const char *onEntitySpawn = "onEntitySpawn";
+    const char *onEntityMove = "onEntityMove";
+    const char *onEntityRotate = "onEntityRotate";
+    const char *onEntityInteractEntity = "onEntityInteractEntity";
+    const char *onEntityInteractBlock = "onEntityInteractBlock";
+    const char *onEntityDamage = "onEntityDamage";
+    const char *onEntityUse = "onEntityUse";
+    const char *onBlockPlace = "onBlockPlace";
+    const char *onBlockDestroy = "onBlockDestroy";
+    const char *onBlockInteract = "onBlockInteract";
+    const char *onInventoryOpen = "onInventoryOpen";
+    const char *onInventoryClose = "onInventoryClose";
+    const char *onInventoryChange = "onInventoryChange";
+    const char *onWorldLoad = "onWorldLoad";
+    const char *onDimensionLoad = "onDimensionLoad";
+    const char *onChunkLoad = "onChunkLoad";
+}
+
+#define onEvent(plugin_manager, key, x) for (const auto &event : plugin_manager._events[key]) { \
+    event(x); \
+}
 
 class PluginManager {
 public:
     PluginManager(std::string &folder);
-    ~PluginManager();
-
-    void getPluginInterface(PluginInterface *pluginInterface);
+    ~PluginManager() = default;
 
     void load(void);
     void reload(void);
 
+
+    std::unordered_map<std::string, std::vector<EventType::AllTypes>> _events;
+
 private:
-    std::string folder;
-    std::unordered_map<std::string, std::vector<std::function<PluginType::EventTypeVariant>>> events;
+    void loadPlugin(std::string filepath);
+
+    std::string _folder;
+    std::shared_ptr<PluginInterface> _interface;
 };
 
 #endif // CUBICSERVER_PLUGINMANAGER_HPP
