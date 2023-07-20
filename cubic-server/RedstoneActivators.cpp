4abae343ead482727a2853b34184d24e08a09f26
QDM <quentin.di-meo@epitech.eu>
2023-07-20 16:56:19 +0200

    initial context for level and button

--- cubic-server/RedstoneActivators.cpp
+++ cubic-server/RedstoneActivators.cpp
@@ -2,8 +2,17 @@
 
 using namespace Redstone;
 
+Lever::Lever(void)
+{
+    // TODO deliver power level 15 to attached and adjacent blocks
+}
+Lever::~Lever(void) {}
+
 Button::Button(std::string material):
     _duration(material.find("stone") != std::string::npos ? 10 : 15); // ticks
 {
     // TODO deliver power level 15 to attached and adjacent blocks
+    // note: a powered button is equivalent to having:
+    //       a redstone block in the button block + a redstone block in lieu of the attached block
 }
+Button::~Button(void) {}
