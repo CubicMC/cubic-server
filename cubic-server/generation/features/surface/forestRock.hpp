ecac7f3d2749dd78c27dc03566c1c722bcc998f4
Ode Marchand <62648027+NekoSelena@users.noreply.github.com>
2023-08-27 13:46:01 +0000

    Add file setup

---
+++ cubic-server/generation/features/surface/forestRock.hpp
@@ -0,0 +1,35 @@
+#ifndef CUBICSERVER_GENERATION_FEATURES_SURFACE_FOREST_ROCK_HPP
+#define CUBICSERVER_GENERATION_FEATURES_SURFACE_FOREST_ROCK_HPP
+
+#include <deque>
+#include <memory>
+#include <vector>
+
+#include "generation/generator.hpp"
+#include "types.hpp"
+#include "world_storage/ChunkColumn.hpp"
+
+using namespace generation;
+
+class ForestRock {
+public:
+    ForestRock(world_storage::ChunkColumn &chunk, Generator &generator):
+        _chunk(chunk),
+        _generator(generator)
+    {
+    }
+
+    std::deque<Position> &getPosForRockGeneration();
+    std::deque<Position> &filterRockOverlap();
+    void generateRock(std::vector<world_storage::ChunkColumn *>);
+
+private:
+    const std::vector<Generator::TreeBlock> getRock(const Position &pos) const;
+    const std::vector<Generator::TreeBlock> getRock(Generator::positionType x, Generator::positionType y, Generator::positionType z) const;
+
+    world_storage::ChunkColumn &_chunk;
+    generation::Generator &_generator;
+    std::deque<Position> _positions;
+};
+
+#endif // CUBICSERVER_GENERATION_FEATURES_SURFACE_FOREST_ROCK_HPP
