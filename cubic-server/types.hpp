1c441c6b68593844a7f770b85fc985a9a5c19564
Trompettesib <alexandre.tissot@epitech.eu>
2023-09-17 13:32:32 +0200

    Fix clang-format

--- cubic-server/types.hpp
+++ cubic-server/types.hpp
@@ -88,7 +88,10 @@ struct Position {
     constexpr bool operator<=(valueType i) const;
 
     template<typename T>
-    constexpr operator Vector3<T>() const { return Vector3<T> {static_cast<T>(x), static_cast<T>(y), static_cast<T>(z)}; }
+    constexpr operator Vector3<T>() const
+    {
+        return Vector3<T> {static_cast<T>(x), static_cast<T>(y), static_cast<T>(z)};
+    }
 
     valueType manhattanDistance(const Position &other) const;
 };
