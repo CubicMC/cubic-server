52c8269f9dacbc623108aeeced3c6b497cec00ac
Michaël Veillet Guillem <micou.veillet@gmail.com>
2023-09-21 13:30:37 +0200

    Fix the parsing of nbt & Silenced a warning due to a bug in gcc

--- cxx_flag_overrides.cmake
+++ cxx_flag_overrides.cmake
@@ -4,7 +4,7 @@ set(CMAKE_CXX_FLAGS_RELEASE_INIT "-O3 -DNDEBUG")
 set(CMAKE_CXX_FLAGS_RELWITHDEBINFO_INIT "-O2 -ggdb3 -fno-omit-frame-pointer -fsanitize=address -fsanitize=null")
 
 if (CMAKE_CXX_COMPILER_ID MATCHES "GNU")
-    set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -Wno-array-bounds")
+    set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -Wno-array-bounds -Wno-stringop-overflow")
 elseif(CMAKE_CXX_COMPILER_ID MATCHES "Clang")
 # elseif(CMAKE_CXX_COMPILER_ID MATCHES "MSVC")
 else()
