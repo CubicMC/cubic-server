87e757fc35f4b75e4b49894d38e104e03f6bd797
Michaël Veillet Guillem <micou.veillet@gmail.com>
2023-09-21 11:44:31 +0200

    Silenced array bound warning beacaus of a bug with gcc

--- cxx_flag_overrides.cmake
+++ cxx_flag_overrides.cmake
@@ -4,6 +4,7 @@ set(CMAKE_CXX_FLAGS_RELEASE_INIT "-O3 -DNDEBUG")
 set(CMAKE_CXX_FLAGS_RELWITHDEBINFO_INIT "-O2 -ggdb3 -fno-omit-frame-pointer -fsanitize=address -fsanitize=null")
 
 if (CMAKE_CXX_COMPILER_ID MATCHES "GNU")
+    set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -Wno-array-bounds")
 elseif(CMAKE_CXX_COMPILER_ID MATCHES "Clang")
 # elseif(CMAKE_CXX_COMPILER_ID MATCHES "MSVC")
 else()
