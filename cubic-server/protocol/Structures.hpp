54aba96cd24104b6ac52ec7053c1c6bf2979b334
Michaël Veillet Guillem <michael.veilletguillem@gmail.com>
2023-11-16 20:33:49 +0100

    Fix nbt being forgor when dropping an item (#245)
    * Fix nbt being forgor when dropping an item
    
    * Removing debug
    
    * Fix undefined behaviour from calling logger in switch statement

--- cubic-server/protocol/Structures.hpp
+++ cubic-server/protocol/Structures.hpp
@@ -1,6 +1,7 @@
 #ifndef CUBICSERVER_PROTOCOL_STRUCTURES_HPP
 #define CUBICSERVER_PROTOCOL_STRUCTURES_HPP
 
+#include <cstddef>
 #include <cstdint>
 
 #include "nbt.h"
@@ -29,6 +30,7 @@ struct Slot {
         nbt(nbt_copy_tag(other.nbt))
     {
     }
+
     Slot &operator=(const Slot &other)
     {
         this->present = other.present;
@@ -37,8 +39,7 @@ struct Slot {
         this->nbt = nbt_copy_tag(other.nbt);
         return *this;
     }
-    // constexpr Slot(Slot &&other) noexcept = default;
-    // constexpr Slot &operator=(Slot &&other) noexcept = default;
+
     constexpr Slot(Slot &&other) noexcept:
         present(other.present),
         itemID(other.itemID),
@@ -59,13 +60,16 @@ struct Slot {
         return *this;
     }
 
+    inline void reset();
+    inline void swap(Slot &other);
+    inline void swap(Slot &other, int8_t count);
+    inline Slot takeOne();
+
+public:
     bool present = false;
     int32_t itemID = 0;
     int8_t itemCount = 0;
     nbt_tag_t *nbt = nullptr;
-    inline void reset();
-    inline void swap(Slot &other);
-    inline void swap(Slot &other, int8_t count);
 };
 
 inline bool operator==(const Slot &lhs, const Slot &rhs) { return lhs.present == rhs.present && lhs.itemID == rhs.itemID && nbt_compare_tags(lhs.nbt, rhs.nbt); }
@@ -111,6 +115,21 @@ void Slot::swap(protocol::Slot &other, int8_t x)
         this->itemCount += other.itemCount - 64;
         other.itemCount = 64;
     }
+}
+
+inline Slot Slot::takeOne()
+{
+    if (this->itemCount == 1) {
+        Slot slot = std::move(*this);
+        nbt = nullptr;
+        this->reset();
+        return slot;
+    }
+    if (this->itemCount > 1) {
+        this->itemCount--;
+        return Slot(true, this->itemID, 1, nbt_copy_tag(this->nbt));
+    }
+    return Slot();
 }
 
 // For chunk and light updates
