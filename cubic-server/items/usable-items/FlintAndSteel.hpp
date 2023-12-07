91be73679a6938ee2d09614d1378b41fb24c71a9
Ode Marchand <62648027+NekoSelena@users.noreply.github.com>
2023-12-07 10:46:51 +0100

    Add "flint and steel" item handling (#251)
    * Update durability handling method (now simplified)
    
    * Add flint and steel (not usable properly yet)
    
    * Update constructors
    
    * Update FlintAndSteel constructor
    
    * Update includes & fix include guard
    
    * Update FlintAndSteel (now working but a bit hardcoded)
    
    * Fix Debug prints and clang-format
    
    * Add documentation
    
    * Fix documentation

---
+++ cubic-server/items/usable-items/FlintAndSteel.hpp
@@ -0,0 +1,32 @@
+#ifndef CUBICSERVER_ITEMS_USABLE_ITEMS_FLINT_AND_STEEL
+#define CUBICSERVER_ITEMS_USABLE_ITEMS_FLINT_AND_STEEL
+
+#include "items/UsableItem.hpp"
+
+namespace Items {
+
+/**
+ * @brief The 'Flint & Steel' is an item used to put fire to everything, especially your friends' houses (and Nether Portals, but that's not really important there)
+ *
+ */
+struct FlintAndSteel : public UsableItem {
+    FlintAndSteel():
+        UsableItem(usableItems[1])
+    {
+    }
+    /**
+     * @note See Item::UsableItem::onUse() for more info
+     *
+     */
+    void onUse(std::shared_ptr<Dimension> dim, Position &pos) const override;
+
+    /**
+     * @note See Item::UsableItem::setNbtTag() for more info
+     *
+     */
+    nbt_tag_t *setNbtTag() override;
+};
+
+} // namespace Items
+
+#endif // CUBICSERVER_ITEMS_USABLE_ITEMS_FLINT_AND_STEEL
