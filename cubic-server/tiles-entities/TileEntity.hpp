0386ee9e914977c409d5ba10875d608baa6a5667
Trompettesib <alexandre.tissot@epitech.eu>
2023-12-22 21:22:50 +0100

    Add TileEntity base class

---
+++ cubic-server/tiles-entities/TileEntity.hpp
@@ -0,0 +1,69 @@
+#ifndef CUBICSERVER_TILEENTITY_HPP
+#define CUBICSERVER_TILEENTITY_HPP
+
+#include "nbt.h"
+#include "protocol/Structures.hpp"
+#include "types.hpp"
+#include <cstdint>
+#include <string>
+
+class TileEntity {
+public:
+    TileEntity(const std::string &id, BlockId blockId, const Position &position, nbt_tag_t *nbt = nullptr):
+        position(position),
+        id(id),
+        blockId(blockId),
+        _nbt(nbt)
+    {
+    }
+    TileEntity(const TileEntity &) = default;
+
+    virtual ~TileEntity()
+    {
+        if (_nbt)
+            nbt_free_tag(_nbt);
+    };
+
+    /**
+     * @brief Update the nbt tag of the tile entity with the current state of the tile entity
+     *
+     * @return TileEntity*
+     */
+    virtual TileEntity *updateNbt() { return this; };
+
+    /**
+     * @brief Get the nbt of the tile entity
+     *
+     * @return const nbt_tag_t*
+     */
+    const nbt_tag_t *getNbt() const { return _nbt; }
+
+    /**
+     * @brief Tick the tile entity
+     */
+    virtual void tick() {};
+
+    /**
+     * @brief Translate the tile entity to a block entity
+     *
+     * @return const protocol::BlockEntity
+     */
+    const protocol::BlockEntity toBlockEntity() const
+    {
+        return protocol::BlockEntity {
+            .sectionCoordinate = int8_t(((position.x & 15) << 4) | (position.z & 15)),
+            .height = int16_t(position.y),
+            .type = blockId,
+            .data = nullptr,
+        };
+    }
+
+    const Position position;
+    const std::string id;
+    const BlockId blockId = 0;
+protected:
+    bool _keepPacked = false;
+    nbt_tag_t *_nbt = nullptr;
+};
+
+#endif // CUBICSERVER_TILEENTITY_HPP
