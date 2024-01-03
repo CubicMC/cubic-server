fb83e8b977fbe14143fd34f8e9a0fdf2ad071e5f
Trompettesib <alexandre.tissot@epitech.eu>
2024-01-03 23:26:25 +0100

    Fix operator Position on Vector3 class

--- cubic-server/math/Vector3.cpp
+++ cubic-server/math/Vector3.cpp
@@ -2,7 +2,13 @@
 #include "types.hpp"
 
 template<>
-constexpr Vector3<double>::operator Position() const noexcept
+Vector3<double>::operator Position() const noexcept
+{
+    return Position {static_cast<Position::valueType>(this->x), static_cast<Position::valueType>(this->y), static_cast<Position::valueType>(this->z)};
+}
+
+template<>
+Vector3<double>::operator Position() noexcept
 {
     return Position {static_cast<Position::valueType>(this->x), static_cast<Position::valueType>(this->y), static_cast<Position::valueType>(this->z)};
 }
