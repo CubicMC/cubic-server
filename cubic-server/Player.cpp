62e5beadf30a9758a2b59f93c40d4d56b89a6ad0
Trompettesib <alexandre.tissot@epitech.eu>
2024-01-07 22:41:55 +0100

    Add a oak_log in the player hotbar at connexion

--- cubic-server/Player.cpp
+++ cubic-server/Player.cpp
@@ -95,7 +95,8 @@ Player::Player(std::weak_ptr<Client> cli, std::shared_ptr<Dimension> dim, u128 u
     this->_inventory->playerInventory().at(16) = protocol::Slot(true, hoe._numeralId, 1, hoeRoot);
     this->_inventory->playerInventory().at(17) = protocol::Slot(true, ITEM_CONVERTER.fromItemToProtocolId("minecraft:wooden_sword"), 1);
     this->_inventory->hotbar().at(0) = protocol::Slot(true, ITEM_CONVERTER.fromItemToProtocolId("minecraft:coal"), 1);
-    this->_inventory->hotbar().at(1) = protocol::Slot(true, ITEM_CONVERTER.fromItemToProtocolId("minecraft:cobblestone"), 10);
+    this->_inventory->hotbar().at(1) = protocol::Slot(true, ITEM_CONVERTER.fromItemToProtocolId("minecraft:oak_log"), 1);
+    this->_inventory->hotbar().at(2) = protocol::Slot(true, ITEM_CONVERTER.fromItemToProtocolId("minecraft:cobblestone"), 10);
     PEXP(incrementPlayerCountGlobal);
 }
 
