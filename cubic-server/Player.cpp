98970e395923bef3b3c6ef0abca775b7684e6ba0
pierrick marguin <pierrick.marguin@epitech.eu>
2024-01-09 19:22:53 +0100

    clang format does clang format correctly :clown:

--- cubic-server/Player.cpp
+++ cubic-server/Player.cpp
@@ -35,9 +35,9 @@
 #include <variant>
 #include <vector>
 
-#define GET_CLIENT()                   \
-    auto client = this -> _cli.lock(); \
-    if (client == nullptr)             \
+#define GET_CLIENT()                 \
+    auto client = this->_cli.lock(); \
+    if (client == nullptr)           \
     return
 
 Player::Player(std::weak_ptr<Client> cli, std::shared_ptr<Dimension> dim, u128 uuid, const std::string &username):
