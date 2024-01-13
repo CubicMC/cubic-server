cba5488120146f820d83cb096354e84715ae3a40
pierrick marguin <pierrick.marguin@epitech.eu>
2024-01-13 21:38:15 +0100

    clang-format

--- cubic-server/world_storage/Section.cpp
+++ cubic-server/world_storage/Section.cpp
@@ -237,8 +237,7 @@ void world_storage::Section::_processBlockRandomTick(uint32_t blockIndex, ChunkC
         utility::farmland(block, chunkColumn, ablsolutePosition);
         LTRACE("Farmland random tick at {}", ablsolutePosition);
     }
-    if (Blocks::Wheat::toProtocol(Blocks::Wheat::Properties::Age::ZERO) <= block &&
-        block <= Blocks::Wheat::toProtocol(Blocks::Wheat::Properties::Age::SEVEN)) {
+    if (Blocks::Wheat::toProtocol(Blocks::Wheat::Properties::Age::ZERO) <= block && block <= Blocks::Wheat::toProtocol(Blocks::Wheat::Properties::Age::SEVEN)) {
         auto ablsolutePosition = world_storage::calculateAbsolutePosition(blockIndex, chunkColumn.getChunkPos(), sectionIndex);
         utility::wheat(block, chunkColumn, ablsolutePosition);
         LTRACE("Wheat random tick at {}", ablsolutePosition);
