bd1982006185625d11267e466007ec3996740907
Alexandre Flion <huntears@kreog.com>
2024-01-13 11:34:20 +0100

    Add simple player/arrow collision
    Currently the arrow goes too fast to collide with the players
    100% of the time, I need some more time to fix that :/

--- cubic-server/entities/Arrow.cpp
+++ cubic-server/entities/Arrow.cpp
@@ -4,6 +4,35 @@
 #include "protocol/ClientPackets.hpp"
 #include "protocol/metadata.hpp"
 
+static inline bool isArrowCollidingPlayer(const Vector3<double> &arrowPosition, const Player &player)
+{
+    // This whole function is fairly verbose, but the compiler will
+    // optimize it out anyway (Or at least I hope, I don't care enough
+    // to test that assumption).
+
+    // The player will always have a standing hitbox
+    // So the hitbox won't change if the player is crouching
+    // I don't think that woud be a problem though as Minecraft
+    // arrows are already kinda scuffed in their own way :3
+    constexpr double playerWidth = 0.6;
+    constexpr double playerHeight = 1.8;
+    constexpr double playerHalfWidth = playerWidth / 2;
+
+    const Vector3<double> &playerPosition = player.getPosition();
+    const double mostZPos = playerPosition.z + playerHalfWidth;
+    const double leastZPos = playerPosition.z - playerHalfWidth;
+    const double mostXPos = playerPosition.x + playerHalfWidth;
+    const double leastXPos = playerPosition.x - playerHalfWidth;
+    const double mostYPos = playerPosition.y + playerHeight;
+    const double leastYPos = playerPosition.y;
+
+    // clang-format off
+    return (arrowPosition.x <= mostXPos && arrowPosition.x >= leastXPos) &&
+        (arrowPosition.y <= mostYPos && arrowPosition.y >= leastYPos) &&
+        (arrowPosition.z <= mostZPos && arrowPosition.z >= leastZPos);
+    // clang-format on
+}
+
 void Arrow::tick()
 {
     constexpr double baseBlocksPerTicks = 1.0 / 8000.0;
@@ -29,6 +58,24 @@ void Arrow::tick()
     const Vector3<double> movement = newVelocity * baseBlocksPerTicks;
     const Vector3<double> newPosition = movement + position;
     this->setPosition(newPosition, false);
+
+    // Collision checks with players
+    {
+        for (auto player : _dim->getPlayers()) {
+            // TODO(huntears): Check gamemode
+            if (isArrowCollidingPlayer(newPosition, *player)) {
+                constexpr float baseArrowDamage = 2.0;
+                const float arrowSpeed = ((float) _velocity.magnitude()) / 8000.0f;
+                const float damageToApply = baseArrowDamage * arrowSpeed;
+
+                // We can assume that the damage source comes from the
+                // previous position of the arrow, as this should give us
+                // the best knockback value.
+                player->attack(damageToApply, position);
+                break;
+            }
+        }
+    }
 
     Entity::tick();
 }
