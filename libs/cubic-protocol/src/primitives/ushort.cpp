69f089f3ee138df65fb3758ecf477b91f0091686
huntears <huntears@kreog.com>
2024-05-26 16:21:19 +0200

    Add protocol primitive ushort

---
+++ libs/cubic-protocol/src/primitives/ushort.cpp
@@ -0,0 +1,130 @@
+#include "cubic-protocol/primitives/ushort.hpp"
+
+namespace cubic::protocol::primitives::ushort {
+
+auto parse(const uint8_t *data, uint32_t available_bytes, uint16_t *value) -> uint32_t
+{
+    if (available_bytes < 2)
+        return 0;
+    *value = data[1] | (data[0] << 8);
+    return 2;
+}
+
+auto parse(uint8_t *const *data, uint32_t available_bytes) -> std::optional<uint16_t>
+{
+    uint16_t result;
+    uint32_t parsed = parse(*data, available_bytes, &result);
+
+    if (parsed == 0)
+        return std::nullopt;
+    return result;
+}
+
+auto parse(const uint8_t *data, uint32_t available_bytes, uint16_t *value, int32_t min, int32_t max)
+    -> uint32_t
+{
+    uint32_t parsed = parse(data, available_bytes, value);
+
+    if (parsed == 0 || *value < min || *value > max)
+        return 0;
+    return parsed;
+}
+
+auto parse(uint8_t *const *data, uint32_t available_bytes, int32_t min, int32_t max)
+    -> std::optional<uint16_t>
+{
+    uint16_t result;
+    uint32_t parsed = parse(*data, available_bytes, &result);
+
+    if (parsed == 0 || result < min || result > max)
+        return std::nullopt;
+    return parsed;
+}
+
+} // namespace cubic::protocol::primitives::ushort
+
+#ifdef UNIT_TESTS
+
+#include <criterion/criterion.h>
+
+TestSuite(primitives_ushort, .timeout = 1);
+
+Test(primitives_ushort, parse_0)
+{
+    using namespace cubic::protocol::primitives::ushort;
+    uint8_t data[] = { 0x00, 0x00 };
+    uint16_t value;
+    uint32_t bytes_parsed = parse(data, sizeof data, &value);
+    cr_assert_eq(bytes_parsed, 2);
+    cr_assert_eq(value, 0);
+}
+
+Test(primitives_ushort, parse_1)
+{
+    using namespace cubic::protocol::primitives::ushort;
+    uint8_t data[] = { 0x00, 0x01 };
+    uint16_t value;
+    uint32_t bytes_parsed = parse(data, sizeof data, &value);
+    cr_assert_eq(bytes_parsed, 2);
+    cr_assert_eq(value, 1);
+}
+
+Test(primitives_ushort, parse_255)
+{
+    using namespace cubic::protocol::primitives::ushort;
+    uint8_t data[] = { 0x00, 0xff };
+    uint16_t value;
+    uint32_t bytes_parsed = parse(data, sizeof data, &value);
+    cr_assert_eq(bytes_parsed, 2);
+    cr_assert_eq(value, 255);
+}
+
+Test(primitives_ushort, parse_256)
+{
+    using namespace cubic::protocol::primitives::ushort;
+    uint8_t data[] = { 0x01, 0x00 };
+    uint16_t value;
+    uint32_t bytes_parsed = parse(data, sizeof data, &value);
+    cr_assert_eq(bytes_parsed, 2);
+    cr_assert_eq(value, 256);
+}
+
+Test(primitives_ushort, parse_25565)
+{
+    using namespace cubic::protocol::primitives::ushort;
+    uint8_t data[] = { 0x63, 0xdd };
+    uint16_t value;
+    uint32_t bytes_parsed = parse(data, sizeof data, &value);
+    cr_assert_eq(bytes_parsed, 2);
+    cr_assert_eq(value, 25565);
+}
+
+Test(primitives_ushort, parse_65535)
+{
+    using namespace cubic::protocol::primitives::ushort;
+    uint8_t data[] = { 0xff, 0xff };
+    uint16_t value;
+    uint32_t bytes_parsed = parse(data, sizeof data, &value);
+    cr_assert_eq(bytes_parsed, 2);
+    cr_assert_eq(value, 65535);
+}
+
+Test(primitives_ushort, parse_fail_almost)
+{
+    using namespace cubic::protocol::primitives::ushort;
+    uint8_t data[] = { 0x00 };
+    uint16_t value;
+    uint32_t bytes_parsed = parse(data, sizeof data, &value);
+    cr_assert_eq(bytes_parsed, 0);
+}
+
+Test(primitives_ushort, parse_fail_nothing)
+{
+    using namespace cubic::protocol::primitives::ushort;
+    uint8_t data[] = {};
+    uint16_t value;
+    uint32_t bytes_parsed = parse(data, sizeof data, &value);
+    cr_assert_eq(bytes_parsed, 0);
+}
+
+#endif
