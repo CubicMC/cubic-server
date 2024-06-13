b2c81178ef5aa18ffef6713cfe89402a0dfccc39
huntears <huntears@kreog.com>
2024-06-13 10:56:44 +0200

    Document string primitive parsing

--- libs/cubic-protocol/include/cubic-protocol/primitives/string.hpp
+++ libs/cubic-protocol/include/cubic-protocol/primitives/string.hpp
@@ -7,7 +7,30 @@
 
 namespace cubic::protocol::primitives::string {
 
+/**
+ * Tries to parse a string
+ *
+ * If the parsing fails then the content of the given value is undefined
+ *
+ * @param data Raw incoming data from a client
+ * @param available_bytes Number of bytes available in the incoming data
+ * @param value Mapping that the function will try to fill
+ * @return The number of bytes used to parse the primitive
+ */
 auto parse(const uint8_t *data, uint32_t available_bytes, std::string *value) -> uint32_t;
+
+/**
+ * Tries to parse a string with size bounds
+ *
+ * If the parsing fails then the content of the given value is undefined
+ *
+ * @param data Raw incoming data from a client
+ * @param available_bytes Number of bytes available in the incoming data
+ * @param value Mapping that the function will try to fill
+ * @param min Minimum value that the parsing should accept from the value
+ * @param mam Maximum value that the parsing should accept from the value
+ * @return The number of bytes used to parse the primitive
+ */
 auto parse(
     const uint8_t *data, uint32_t available_bytes, std::string *value, size_t min, size_t max
 ) -> uint32_t;
