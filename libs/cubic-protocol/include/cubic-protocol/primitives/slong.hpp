df46bd11985e27908d42400a4482b67ad4cbe644
huntears <huntears@kreog.com>
2024-06-13 10:54:06 +0200

    Document signed long primitive parsing

--- libs/cubic-protocol/include/cubic-protocol/primitives/slong.hpp
+++ libs/cubic-protocol/include/cubic-protocol/primitives/slong.hpp
@@ -5,7 +5,30 @@
 
 namespace cubic::protocol::primitives::slong {
 
+/**
+ * Tries to parse a signed long
+ *
+ * If the parsing fails then the content of the given value is undefined
+ *
+ * @param data Raw incoming data from a client
+ * @param available_bytes Number of bytes available in the incoming data
+ * @param value Mapping that the function will try to fill
+ * @return The number of bytes used to parse the primitive
+ */
 auto parse(const uint8_t *data, uint32_t available_bytes, int64_t *value) -> uint32_t;
+
+/**
+ * Tries to parse a signed long with bounds
+ *
+ * If the parsing fails then the content of the given value is undefined
+ *
+ * @param data Raw incoming data from a client
+ * @param available_bytes Number of bytes available in the incoming data
+ * @param value Mapping that the function will try to fill
+ * @param min Minimum value that the parsing should accept from the value
+ * @param mam Maximum value that the parsing should accept from the value
+ * @return The number of bytes used to parse the primitive
+ */
 auto parse(const uint8_t *data, uint32_t available_bytes, int64_t *value, int64_t min, int64_t max)
     -> uint32_t;
 
