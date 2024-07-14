2bef97ad573338d6ae0da855fb54e33bb3617f92
emneo <emneo@kreog.com>
2024-07-14 12:25:32 +0200

    Remove cubic-protocol

--- libs/cubic-protocol/include/cubic-protocol/primitives.hpp
+++
@@ -1,9 +0,0 @@
-#ifndef CUBIC_PROTOCOL_PRIMITIVES_
-#define CUBIC_PROTOCOL_PRIMITIVES_
-
-#include "cubic-protocol/primitives/slong.hpp"
-#include "cubic-protocol/primitives/string.hpp"
-#include "cubic-protocol/primitives/ushort.hpp"
-#include "cubic-protocol/primitives/varint.hpp"
-
-#endif // CUBIC_PROTOCOL_PRIMITIVES_
